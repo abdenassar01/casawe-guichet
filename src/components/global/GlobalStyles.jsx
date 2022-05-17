2a9608e672ec2e31005b2d0122c9caf812e6298f
abdenassar01 <abdenassaramimi99@gmail.com>
2022-05-17 16:22:28 +0100

    initial commit

---
+++ src/components/global/GlobalStyles.jsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: Roboto,sans-serif;
+  }
+
+  body{
+    background: #f2f5f7;
+  }
+`;
+ 
+export default GlobalStyle;
