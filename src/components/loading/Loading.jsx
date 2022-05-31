86ce3ee05510ae1496f8d72318ef6adca2c2c4ce
abdenassar01 <abdenassaramimi99@gmail.com>
2022-05-31 11:45:50 +0100

    making a loading component

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,21 @@
+import styledComponents from "styled-components"
+import BeatLoader from "react-spinners/BeatLoader"
+
+const Loading = () => {
+  return (
+    <Wrapper>
+        <BeatLoader color={"#003C72"} loading={true} css={{}} size={50}/>
+    </Wrapper>
+  )
+}
+
+export default Loading
+
+const Wrapper = styledComponents.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 100px 0;
+`
